c5152b951dfbe5db5ecbc219f2be0e3d8a885dc0
Raj Patil <rxjpatil@gmail.com>
2024-04-30 18:39:19 +0000

    Variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -10,7 +10,7 @@ console.log(percentage);
 let student = "Raj Patil"
 std_id = "69"
 branch = "CSE"
-let presenty;
-console.table([student,std_id,branch,percentage,marks,bank_id, presenty])
+let attendence;
+console.table([student,std_id,branch,percentage,marks,bank_id, attendence])
 
 // Prefer not to use "var" because os issue in block scope
