7cc1ea580577f9773d64d07986cc9fbcb5a5a34e
Raj Patil <rxjpatil@gmail.com>
2024-04-30 14:28:20 +0000

    new information added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const marks = 616
+// marks = 717 // not allowed in js
+console.log(marks);
+
+let percentage = 61
+percentage = 87
+var bank_id = 5646
+console.log(percentage);
+
+let student = "Raj Patil"
+std_id = "69"
+branch = "CSE"
+
+console.table([student,std_id,branch,percentage,marks,bank_id])
+
+// Prefer not to use "var" because os issue in block scope
