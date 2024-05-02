479d9913b40cae5027748e2638581203f05ac265
Raj Patil <rxjpatil@gmail.com>
2024-05-02 10:45:16 +0000

    Container config done

---
+++ Practice Questions/01_practice.js
@@ -0,0 +1,20 @@
+// Declare your name as a string and print its length in JavaScript 
+name = "Raj Patil"
+console.log(name.length);
+
+// Declare your name as a string and print its first character 
+console.log(name[0]);
+
+// Declare your name as a string and print its last character 
+console.log(name[8]);
+
+// What is the output of the following  code :
+
+// "raj"+7
+console.log("rxj"+7);
+
+// What are lengths of an empty string and a string with a single space
+a = "" //0
+b = " "  //1
+
+console.log(a.length, b.length);
