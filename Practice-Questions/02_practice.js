753545528b6c197e3d8dfafad412b86bdff274b4
Raj Patil <rxjpatil@gmail.com>
2024-05-03 18:38:05 +0000

    Commit config done

--- Practice-Questions/02_practice.js
+++ Practice-Questions/02_practice.js
@@ -60,3 +60,32 @@ switch(colour){
         console.log("broken light");
 
 }
+
+// Use Switch case statement to print the day of the week using a number variable 'day' with value 1 to 7.
+// 1 = Monday , 2 = Tuesday and so on..... 
+day = 7
+switch (day){
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+        console.log("Enter valid day");
+}
